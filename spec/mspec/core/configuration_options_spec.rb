57e8ba5deecda27247724fc4f7ebbd3d0cbfc8cc
Sasha <sashainkiev@gmail.com>
2011-11-28 22:58:02 +0200

    files_or_directories_to_run: parses dir from 'dir'

--- spec/mspec/core/configuration_options_spec.rb
+++ spec/mspec/core/configuration_options_spec.rb
@@ -175,6 +175,10 @@ describe MSpec::Core::ConfigurationOptions do
       parse_options("-c", "file.rb", "dir/file.rb").should include(:files_or_directories_to_run => ["file.rb", "dir/file.rb"])
     end
 
+    it "parses dir from 'dir'" do
+      parse_options("dir").should include(:files_or_directories_to_run => ["dir"])
+    end
+
     it "provides no files or directories if spec directory does not exist" do
       File.stub(:directory?).with("spec").and_return true
       parse_options()[:files_or_directories_to_run].should be_empty
