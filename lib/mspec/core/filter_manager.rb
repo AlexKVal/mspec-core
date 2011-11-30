0a3080353284c510b729ea49877b5fc53556d3cd
Sasha <sashainkiev@gmail.com>
2011-12-01 00:28:05 +0200

    did STANDALONE_FILTERS

--- lib/mspec/core/filter_manager.rb
+++ lib/mspec/core/filter_manager.rb
@@ -1,6 +1,8 @@
 module MSpec
   module Core
     class FilterManager
+      STANDALONE_FILTERS = [:locations, :line_numbers, :full_description]
+
       attr_reader :exclusions, :inclusions
 
       def initialize
@@ -8,6 +10,19 @@ module MSpec
       end
 
       def include(*args)
+        # args.each do |arg|
+        #   [:line_numbers, :locations, :full_description].includes? arg.key
+        #   args.unshift :replace
+        # end
+
+        puts "#{'*'*20}"
+        puts "args: " + args.inspect
+
+        hash_arg = args.first.is_a?(Hash) ? args.first : args.last
+        args.unshift :replace if STANDALONE_FILTERS.any? do |name|
+          hash_arg.has_key? name
+        end
+
         update(@inclusions, @exclusions, *args)
       end
 
@@ -16,17 +31,22 @@ module MSpec
       end
 
       def update(orig, opposite, *updates)
+        puts "#{'='*15}"
         puts "orig: " + orig.inspect
         puts "opposite: " + opposite.inspect
         puts "updates: " + updates.inspect
 
-        if updates.first == :weak
+        case updates.first
+        when :replace
+          orig.replace(updates.last)
+        when :weak # priority
           updated = updates.last.merge(orig)
           opposite.each_key {|k| updated.delete(k)}
           orig.replace(updated)
-        else
+        else # strong priority
           orig.merge!(updates.last).each_key {|k| opposite.delete(k)}
         end
+
         puts "orig: " + orig.inspect
       end
     end
--- spec/mspec/core/filter_manager_spec.rb
+++ spec/mspec/core/filter_manager_spec.rb
@@ -18,7 +18,7 @@ module MSpec::Core
         filter_manager.send name, :foo => 2
         filter_manager.send(type).should eq(:foo => 2)
       end
-      
+
       it "ignores new #{type} if same key exists and priority is low" do
         filter_manager = FilterManager.new
         filter_manager.exclusions.clear # defaults
@@ -28,25 +28,25 @@ module MSpec::Core
       end
     end
 
-    # it "clears the inclusion filter on include :line_numbers" do
-    #   filter_manager = FilterManager.new
-    #   filter_manager.include :foo => :bar
-    #   filter_manager.include :line_numbers => [100]
-    #   filter_manager.inclusions.should eq(:line_numbers => [100])
-    # end
-    # 
-    # it "clears the inclusion filter on include :locations" do
-    #   filter_manager = FilterManager.new
-    #   filter_manager.include :foo => :bar
-    #   filter_manager.include :locations => { "path/to/file.rb" => [37] }
-    #   filter_manager.inclusions.should eq(:locations => { "path/to/file.rb" => [37] })
-    # end
-    # 
-    # it "clears the inclusion filter on include :full_description" do
-    #   filter_manager = FilterManager.new
-    #   filter_manager.include :foo => :bar
-    #   filter_manager.include :full_description => "this and that"
-    #   filter_manager.inclusions.should eq(:full_description => "this and that")
-    # end
+    it "clears the inclusion filter on include :line_numbers" do
+      filter_manager = FilterManager.new
+      filter_manager.include :foo => :bar
+      filter_manager.include :line_numbers => [100]
+      filter_manager.inclusions.should eq(:line_numbers => [100])
+    end
+
+    it "clears the inclusion filter on include :locations" do
+      filter_manager = FilterManager.new
+      filter_manager.include :foo => :bar
+      filter_manager.include :locations => { "path/to/file.rb" => [37] }
+      filter_manager.inclusions.should eq(:locations => { "path/to/file.rb" => [37] })
+    end
+
+    it "clears the inclusion filter on include :full_description" do
+      filter_manager = FilterManager.new
+      filter_manager.include :foo => :bar
+      filter_manager.include :full_description => "this and that"
+      filter_manager.inclusions.should eq(:full_description => "this and that")
+    end
   end
 end
