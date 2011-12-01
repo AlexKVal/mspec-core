266011c9817d35944470dabb9ba53f5eace505ba
Sasha <sashainkiev@gmail.com>
2011-12-01 16:41:35 +0200

    Metadata#process sketch

--- lib/mspec/core/metadata.rb
+++ lib/mspec/core/metadata.rb
@@ -1,4 +1,32 @@
 module MSpec::Core
   class Metadata < Hash
+    def initialize
+      self[:example_group] = {}
+    end
+    def process(*args)
+      RESERVED_KEYS.each do |key|
+        raise ArgumentError.new(":#{key} is not allowed") if args[1].has_key? key
+      end
+
+      if args.first == 'group'
+        puts "args[1][:caller]: " + args[1][:caller].inspect #remove
+        
+        self[:example_group][:location] = args[1][:caller].first if args[1].has_key? :caller
+        
+        puts "self[:example_group][:location]: " + self[:example_group][:location].inspect #remove
+      end
+    end
+
+    private
+
+      RESERVED_KEYS = [
+        :description,
+        :example_group,
+        :execution_result,
+        :file_path,
+        :full_description,
+        :line_number,
+        :location
+      ]
   end
 end
--- spec/mspec/core/metadata_spec.rb
+++ spec/mspec/core/metadata_spec.rb
@@ -3,6 +3,25 @@ require 'spec_helper'
 module MSpec
   module Core
     describe Metadata do
+
+      describe "#process" do
+        Metadata::RESERVED_KEYS.each do |key|
+          it "prohibits :#{key} as a hash key" do
+            m = Metadata.new
+            expect do
+              m.process('group', key => {})
+            end.to raise_error(/:#{key} is not allowed/)
+          end
+        end
+
+        it "uses :caller if passed as part of the user metadata" do
+          m = Metadata.new
+          m.process('group', :caller => ['example_file:42'])
+          m[:example_group][:location].should eq("example_file:42")
+        end
+      end
+
+
     end
   end
 end
